206bba241301cf9abe44a84a80e9088e309b0857
Florian Boireau <florian23russie@hotmail.fr>
2022-03-21 17:07:33 +0100

    Finished the Clicker mecanic

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Clicker : MonoBehaviour
 {
+    [SerializeField] private TMP_Text text;
+
+    private List<string>     lOrdersString;
+    private List<GameObject> lOrdersObject;
+
+    private int missionNumber = 0;
+
+    private void Start()
+    {
+        lOrdersString = new List<string>();
+        lOrdersString.Add("Trouve le tricorne");
+        lOrdersString.Add("Trouve la chaussure volante");
+        lOrdersString.Add("Trouve l'ètre apeuré");
+
+        lOrdersObject = new List<GameObject>();
+        lOrdersObject.Add(GameObject.Find("Tricorn"));
+        lOrdersObject.Add(GameObject.Find("Shoe"));
+        lOrdersObject.Add(GameObject.Find("Kid Head"));
+
+        text.text = lOrdersString[missionNumber];
+    }
+
     private void Update()
     {
         MouseListener();
@@ -16,11 +39,20 @@ public class Clicker : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hit.collider != null)
             {
-                if (hit.collider.GetType() == typeof(BoxCollider2D))
+                if (hit.collider.GetType() == typeof(BoxCollider2D) && hit.collider.transform.gameObject.name == lOrdersObject[missionNumber].name)
                 {
-                    Debug.Log("yes");
+                    missionStepComplete();
                 }
             }
         }
+    }
+
+    private void missionStepComplete()
+    {
+        missionNumber++;
+        if (lOrdersObject.Count > missionNumber)
+            text.text = lOrdersString[missionNumber];
+        else
+            text.text = "niveau fini";
     }
 }
