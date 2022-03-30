1cf069cb94131fec58ffc8699cf8c898952ae097
Florian Boireau <florian23russie@hotmail.fr>
2022-03-30 07:24:05 +0200

    Question script base

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Question : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FalseAnswer()
+    {
+
+    }
+
+    public void TrueAnswer()
+    {
+
+    }
+}
