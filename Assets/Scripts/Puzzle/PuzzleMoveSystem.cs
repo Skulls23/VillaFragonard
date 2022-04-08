88dcbd19f46e13e0fc081853561d51199954a1a1
Florian Boireau <florian23russie@hotmail.fr>
2022-04-08 11:39:37 +0200

    Created a puzzle2 scene which is supposed to be a better version
    code done

---
+++ Assets/Scripts/Puzzle/PuzzleMoveSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleMoveSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject correctForm;
+    private bool moving;
+
+    private float startPosX;
+    private float startPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moving)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.position.x;
+            startPosY = mousePos.y - this.transform.position.y;
+
+            moving = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        moving = false;
+    }
+}
