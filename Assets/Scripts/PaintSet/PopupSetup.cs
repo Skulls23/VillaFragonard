010ab72bcba92f5be101bfe60f499fb8493560f9
Florian Boireau <florian23russie@hotmail.fr>
2022-05-19 16:34:29 +0200

    updated popup for resolution

--- Assets/Scripts/PaintSet/PopupSetup.cs
+++ Assets/Scripts/PaintSet/PopupSetup.cs
@@ -11,12 +11,13 @@ public class PopupSetup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        popup = GameObject.FindGameObjectsWithTag("Popup")[0];
+        popup = GameObject.FindGameObjectsWithTag("Popup")[1];
         popup.SetActive(false);
     }
 
     public void RevealPopup(string title, string description, Sprite sprite)
     {
+        Debug.Log(sprite.name);
         popup.transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = sprite;
         popup.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = title;
         popup.transform.GetChild(0).GetChild(1).GetChild(1).GetComponent<TMP_Text>().text = description;
