c0456f107696b82e1f18b5ad5b63ec2e9556108f
Florian Boireau <florian23russie@hotmail.fr>
2022-06-01 08:48:39 +0200

    Dejeuner du chat (DDC) movement fixed

--- Assets/Scripts/Slider/MoveTile.cs
+++ Assets/Scripts/Slider/MoveTile.cs
@@ -35,21 +35,21 @@ public class MoveTile : MonoBehaviour
         float startXMinusNewX = Mathf.Abs(startPosX) - Mathf.Abs(mousePos.x);
         float startYMinusNewY = Mathf.Abs(startPosY) - Mathf.Abs(mousePos.y);
 
-        if      (startXMinusNewX <= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //cursor moved to right-up but more up than right
+        if      (startXMinusNewX <= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //Cursor moved to right-up but more up than right
             MoveInHierarchy("up");
-        else if (startXMinusNewX <= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //cursor moved to right-up but more right than up
+        else if (startXMinusNewX <= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //Cursor moved to right-up but more right than up
             MoveInHierarchy("right");
-        else if (startXMinusNewX <= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //cursor moved to right-down but more down than right
+        else if (startXMinusNewX <= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //Cursor moved to right-down but more down than right
             MoveInHierarchy("down");
-        else if (startXMinusNewX <= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //cursor moved to right-down but more right than down
+        else if (startXMinusNewX <= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //Cursor moved to right-down but more right than down
             MoveInHierarchy("right");
-        else if (startXMinusNewX >= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //cursor moved to left-up but more up than left
+        else if (startXMinusNewX >= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //Cursor moved to left-up but more up than left
             MoveInHierarchy("up");
-        else if (startXMinusNewX >= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //cursor moved to left-up but more left than up
+        else if (startXMinusNewX >= 0 && startYMinusNewY <= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //Cursor moved to left-up but more left than up
             MoveInHierarchy("left");
-        else if (startXMinusNewX >= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //cursor moved to left-down but more down than left
+        else if (startXMinusNewX >= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) < Mathf.Abs(startYMinusNewY)) //Cursor moved to left-down but more down than left
             MoveInHierarchy("down");
-        else if (startXMinusNewX >= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //cursor moved to left-down but more left than down
+        else if (startXMinusNewX >= 0 && startYMinusNewY >= 0 && Mathf.Abs(startXMinusNewX) > Mathf.Abs(startYMinusNewY)) //Cursor moved to left-down but more left than down
             MoveInHierarchy("left");
         else
             print("static");
@@ -68,40 +68,32 @@ public class MoveTile : MonoBehaviour
 
     private void MoveInHierarchy(string direction)
     {
-        print(direction);
         int positionInHierarchy = transform.GetSiblingIndex();
+        print((positionInHierarchy % 5));
 
         if (direction == "up")
-        {
-            if (positionInHierarchy >= 0)
+            if (positionInHierarchy > 4) //Can't move up the first 5 first childs 
             {
                 transform.parent.GetChild(positionInHierarchy - 5).SetSiblingIndex(positionInHierarchy);
                 transform.SetSiblingIndex(positionInHierarchy - 5);
             }
-        }
         else if (direction == "right")
-        {
-            if (positionInHierarchy + 1 <= MAX_TILE_NUMBER - 1)
+            if (positionInHierarchy % 5 != 4) //The pieces on right can't move right
             {
                 transform.parent.GetChild(positionInHierarchy + 1).SetSiblingIndex(positionInHierarchy);
                 transform.SetSiblingIndex(positionInHierarchy + 1);
             }
-        }
         else if (direction == "down")
-        {
-            if (positionInHierarchy + 5 <= MAX_TILE_NUMBER - 1)
+            if (positionInHierarchy + 5 <= MAX_TILE_NUMBER - 1) //Verify if the future position is in the limit
             {
                 transform.parent.GetChild(positionInHierarchy + 5).SetSiblingIndex(positionInHierarchy);
                 transform.SetSiblingIndex(positionInHierarchy + 5);
             }
-        }
         else if (direction == "left")
-        {
-            if (positionInHierarchy - 1 >= 0)
+            if (positionInHierarchy % 5 != 0) //The pieces on left can't move left
             {
                 transform.parent.GetChild(positionInHierarchy - 1).SetSiblingIndex(positionInHierarchy);
                 transform.SetSiblingIndex(positionInHierarchy - 1);
             }
-        }
     }
 }
