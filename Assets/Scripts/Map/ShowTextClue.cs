db15537f15be7f175cd63f70ea95205a89e8c381
Florian Boireau <florian23russie@hotmail.fr>
2022-07-05 15:02:51 +0200

    fixed a bug with the paint set new button

--- Assets/Scripts/Map/ShowTextClue.cs
+++ Assets/Scripts/Map/ShowTextClue.cs
@@ -8,16 +8,20 @@ public class ShowTextClue : MonoBehaviour
     [SerializeField] string[] clues;
 
     private GameObject popup;
+    private GameObject mapButton;
     // Awake is called before all the Start
     void Awake()
     {
         popup = GameObject.FindGameObjectWithTag("Popup");
         popup.SetActive(false);
+        mapButton = GameObject.FindGameObjectWithTag("MapButton");
+        mapButton.SetActive(false);
     }
 
     public void ShowClue(int clueNumber)
     {
         popup.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = clues[clueNumber];
         popup.SetActive(true);
+        mapButton.SetActive(true);
     }
 }
