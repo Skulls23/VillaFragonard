6347aa08001c0633210975cc897c3ab45fbf52c2
Florian Boireau <florian23russie@hotmail.fr>
2022-07-11 09:50:21 +0200

    We can now see again the clue text once it already revealed once

---
+++ Assets/Scripts/Map/ClueLogoClicked.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClueLogoClicked : MonoBehaviour
+{
+    public void ShowClue()
+    {
+        if(gameObject.GetComponent<Image>().color.a == 1f)
+            GameObject.Find("Gameplay").GetComponent<ShowTextClue>().ShowClue(transform.GetSiblingIndex());
+    }
+}
--- Assets/Scripts/Map/CluesRevealer.cs
+++ Assets/Scripts/Map/CluesRevealer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// This script manage the letter-clues on the Map. <br/>
@@ -10,8 +11,9 @@ public class CluesRevealer : MonoBehaviour
 {
     private static int NUMBER_OF_CLUES_TO_FIND = 7;
 
-    private static string[] aLetter = { "F", "R", "A", "N", "C", "M", "O" };
     [SerializeField] private List<GameObject> lClues = new List<GameObject>();
+    private static string[] aLetter = { "F", "R", "A", "N", "C", "M", "O" };
+    private GameObject[] aCluesButton;
     private int numberOfCluesRevealed = 0;
 
     // Start is called before the first frame update
@@ -23,11 +25,20 @@ public class CluesRevealer : MonoBehaviour
             PlayerPrefs.SetInt("Number of clues unlocked", 0);
         }
 
+        //The button to see again the clue text must disappear
+        aCluesButton = new GameObject[aLetter.Length];
+        for(int i=0; i < aLetter.Length; i++)
+        {
+            aCluesButton[i] = GameObject.Find("Clue Text (" + i + ")");
+            aCluesButton[i].GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        }
+
         for (int i = 0; i < lClues.Count; i++)
             if (PlayerPrefs.HasKey(lClues[i].name) && PlayerPrefs.GetInt(lClues[i].name) == 1)
             {
                 lClues[i].SetActive(true);
                 GameObject.Find(lClues[i].name + " collider").SetActive(false); //disable the collider bound to that clue
+                aCluesButton[i].GetComponent<Image>().color = new Color(255, 255, 255, 1f); //the button to the clue text again reappear
             }
             else
                 lClues[i].SetActive(false);
