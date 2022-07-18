b338f8caea93bdfe03291cd3089fc0d5a3960499
Florian Boireau <florian23russie@hotmail.fr>
2022-07-18 14:10:17 +0200

    The main menu hide the copies button if reset | Changed map return spot

--- Assets/Scripts/ResetSave.cs
+++ Assets/Scripts/ResetSave.cs
@@ -4,17 +4,20 @@ public class ResetSave : MonoBehaviour
 {
     private GameObject stairsButton;
     private GameObject reserveButton;
+    private GameObject copiesButton;
 
     private void Awake()
     {
         stairsButton  = GameObject.Find("Stairs");
         reserveButton = GameObject.Find("Reserve");
+        copiesButton  = GameObject.Find("Copies");
     }
 
     public void Reset()
     {
         stairsButton.SetActive (false);
         reserveButton.SetActive(false);
+        copiesButton.SetActive (false);
         PlayerPrefs.DeleteAll  ();
     }
 }
