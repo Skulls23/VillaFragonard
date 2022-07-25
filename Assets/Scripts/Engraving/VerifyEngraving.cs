cc897b5c1fc396637ddee0170f764219bac75773
Florian Boireau <florian23russie@hotmail.fr>
2022-07-25 12:31:24 +0200

    The buton now changes color

--- Assets/Scripts/Engraving/VerifyEngraving.cs
+++ Assets/Scripts/Engraving/VerifyEngraving.cs
@@ -8,7 +8,13 @@ public class VerifyEngraving : MonoBehaviour
     [SerializeField] private int numberToWin = 3;
     [SerializeField] private string clueName;
     private int numCorrect;
-    
+    private Color baseColor;
+
+    private void Awake()
+    {
+        baseColor = GetComponent<Image>().color;
+    }
+
     public void VerifySprites()
     {
 
@@ -20,8 +26,15 @@ public class VerifyEngraving : MonoBehaviour
             numCorrect++;
             go[0].GetComponent<Swap>().DeleteActualSprite();
             go[1].GetComponent<Swap>().DeleteActualSprite();
+            transform.GetComponent<Image>().color = Color.green;
+            StartCoroutine(ReturnColorToNormal(0.6f));
         }
-        print(numCorrect + " " + numberToWin);
+        else
+        {
+            transform.GetComponent<Image>().color = Color.red;
+            StartCoroutine(ReturnColorToNormal(0.6f));
+        }
+        
         if (numCorrect == numberToWin)
         {
             print("win");
@@ -29,4 +42,11 @@ public class VerifyEngraving : MonoBehaviour
             PlayerPrefs.SetInt(clueName, PlayerPrefs.GetInt("Number of clues unlocked"));
         }
     }
+
+    IEnumerator ReturnColorToNormal(float f)
+    {
+        yield return new WaitForSeconds(f);
+        transform.GetComponent<Image>().color = baseColor;
+    }
+
 }
