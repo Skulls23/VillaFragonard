af701741c2a6278d4d93560fef8715cd9d263061
Florian Boireau <florian23russie@hotmail.fr>
2022-07-25 12:22:26 +0200

    Engraving finish, just need to place popup and do buton color

--- Assets/Scripts/Engraving/Swap.cs
+++ Assets/Scripts/Engraving/Swap.cs
@@ -24,28 +24,51 @@ public class Swap : MonoBehaviour
         float newPosY = Input.mousePosition.y;
         
         if (newPosY >= startPosY)
-            SwapSprite("up");
-        else if (newPosY <= startPosY)
-            SwapSprite("down");
-        else
-            print("static");
-    }
-    
-    private void SwapSprite(string str)
-    {
-        if (str == "up")
         {
             spriteNumber++;
             if (spriteNumber >= aSprite.Length)
                 spriteNumber = 0;
+            SwapSprite();
         }
-        else if (str == "down")
+        else if (newPosY <= startPosY)
         {
             spriteNumber--;
-            if (spriteNumber < 0)
-                spriteNumber = aSprite.Length-1;
+            
+            SwapSprite();
         }
+            
+        else
+            print("static");
+    }
+    
+    private void SwapSprite()
+    {
+        if (aSprite.Length != 0)
+        {
+            if (spriteNumber < 0)
+                spriteNumber = aSprite.Length - 1;
+            else if (spriteNumber >= aSprite.Length)
+                spriteNumber = 0;
 
-        GetComponent<Image>().sprite = aSprite[spriteNumber];
+            GetComponent<Image>().sprite = aSprite[spriteNumber];
+        }
+    }
+    
+    
+
+    public void DeleteActualSprite()
+    {
+        if (aSprite.Length != 0)
+        {
+            Sprite[] aSpriteReplacer = new Sprite[aSprite.Length - 1];
+            int placement = 0;
+            for (int i = 0; i < aSprite.Length; i++)
+            {
+                if (i != spriteNumber)
+                    aSpriteReplacer[placement++] = aSprite[i];
+            }
+            aSprite = aSpriteReplacer;
+            SwapSprite();
+        }
     }
 }
--- Assets/Scripts/Engraving/VerifyEngraving.cs
+++ Assets/Scripts/Engraving/VerifyEngraving.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VerifyEngraving : MonoBehaviour
 {
+    [SerializeField] private int numberToWin = 3;
+    [SerializeField] private string clueName;
+    private int numCorrect;
+    
     public void VerifySprites()
     {
-        
+
+        GameObject[] go = GameObject.FindGameObjectsWithTag("Piece");
+
+
+        if (go[0].GetComponent<Image>().sprite.name == go[1].GetComponent<Image>().sprite.name)// + "Print")
+        {
+            numCorrect++;
+            go[0].GetComponent<Swap>().DeleteActualSprite();
+            go[1].GetComponent<Swap>().DeleteActualSprite();
+        }
+        print(numCorrect + " " + numberToWin);
+        if (numCorrect == numberToWin)
+        {
+            print("win");
+            PlayerPrefs.SetInt("Number of clues unlocked", PlayerPrefs.GetInt("Number of clues unlocked") + 1);
+            PlayerPrefs.SetInt(clueName, PlayerPrefs.GetInt("Number of clues unlocked"));
+        }
     }
 }
