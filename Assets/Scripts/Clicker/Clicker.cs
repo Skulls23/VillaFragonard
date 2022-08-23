1f045e8dc08c9e2292408694be05ffae654e22e7
Florian Boireau <florian23russie@hotmail.fr>
2022-08-23 10:38:40 +0200

    Modified last lavandiere text

--- Assets/Scripts/Clicker/Clicker.cs
+++ Assets/Scripts/Clicker/Clicker.cs
@@ -23,7 +23,7 @@ public class Clicker : MonoBehaviour
         text.text = lOrderStrings[missionNumber];
 
         lTextPopUp[4] = "Ce tableau a connu un grand succès au 18ème siècle. Acheté par Louis 18, c’est la seconde œuvre de Jean-Honoré Fragonard à intégrer les collections du Musée du Louvre." + Environment.NewLine +
-                        "Dans ce paysage, Fragonard ajoute des détails qui font référence aux paysages des peintres Hollandais du 17ème siècle. La présence des lavandières fait passer le tableau d’un genre de peinture*à un autre : de paysage à scène de genre.";
+                        "Dans ce paysage, Fragonard ajoute des détails qui font référence aux paysages des peintres Hollandais du 17ème siècle. La présence des lavandières fait passer le tableau d’un genre de peinture à un autre : de paysage à scène de genre.";
     }
 
     public void ButtonClicked(int numButton)
