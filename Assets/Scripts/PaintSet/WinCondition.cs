b0fadbc37550d9ef8db728dbabd38b4357b6d6db
Florian Boireau <florian23russie@hotmail.fr>
2022-08-23 09:37:01 +0200

    Added the number of the clue

--- Assets/Scripts/PaintSet/WinCondition.cs
+++ Assets/Scripts/PaintSet/WinCondition.cs
@@ -36,7 +36,6 @@ public class WinCondition : MonoBehaviour
             PlayerPrefs.SetInt(clueName, PlayerPrefs.GetInt("Number of clues unlocked"));
             popUpEnd.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Cette boîte à couleurs a sans doute appartenu à Jean - Honoré Fragonard.Les flacons contiennent des pigments en poudre de différentes couleurs." + Environment.NewLine +
                                                                                    "Pour faire de la peinture, on mélange un pigment avec un liant.Selon le type de peinture souhaité, le liant n’est pas le même : un jaune d’œuf pour faire de la peinture a tempera, de l’huile de lin pour faire de la peinture à l’huile…";
-;
             popUpEnd.SetActive(true);
         }
     }
