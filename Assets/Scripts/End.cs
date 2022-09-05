c48415831424714065d7d9d78d66483943628277
Florian Boireau <florian23russie@hotmail.fr>
2022-09-05 11:31:25 +0200

    Brutus rule and text changed

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -20,6 +20,7 @@ public class End : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        popUpText = popUpText.Replace("\\n", "\n");
         popup.SetActive(false);
     }
 
