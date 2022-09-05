83d1d1a59914ea1264874541e51f0f07929af1cb
Florian Boireau <florian23russie@hotmail.fr>
2022-09-05 15:17:20 +0200

    Fixed some bug

--- Assets/Scripts/PaintSet/WinCondition.cs
+++ Assets/Scripts/PaintSet/WinCondition.cs
@@ -38,10 +38,10 @@ public class WinCondition : MonoBehaviour
             PlayerPrefs.SetInt("Number of clues unlocked", PlayerPrefs.GetInt("Number of clues unlocked") + 1);
             PlayerPrefs.SetInt(clueName, PlayerPrefs.GetInt("Number of clues unlocked"));
             popUpEnd.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Cette boîte à couleurs m’a appartenu. Les flacons contiennent des pigments en poudre de différentes couleurs." + Environment.NewLine +
-                                                                                   "Pour faire de la peinture, on mélange un pigment avec un liant.Selon le type de peinture souhaité, le liant n’est pas le même : " +
+                                                                                   "Pour faire de la peinture, on mélange un pigment avec un liant. Selon le type de peinture souhaité, le liant n’est pas le même : " +
                                                                                    "un jaune d’œuf pour faire de la peinture a tempera, de l’huile de lin pour faire de la peinture à l’huile…";
             popUpEnd.SetActive(true);
-            popUpRule.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "La boîte à couleurs est complétée. Double-cliquer sur un élément pour en savoir plus." + Environment.NewLine +
+            popUpRule.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "La boîte à couleurs est complétée.\n Double-cliquer sur un élément pour en savoir plus." + Environment.NewLine +
                                                                                     "Cliquer sur la flèche du bas pour revenir sur le plan et poursuivre votre parcours.";
             popUpRule.SetActive(true);
         }
