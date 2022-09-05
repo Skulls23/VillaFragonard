6a209e001cfd2061ebf15f763dd9818844a36aab
Florian Boireau <florian23russie@hotmail.fr>
2022-09-05 11:14:37 +0200

    PaintsetMinigame changed text

--- Assets/Scripts/PaintSet/WinCondition.cs
+++ Assets/Scripts/PaintSet/WinCondition.cs
@@ -8,12 +8,15 @@ public class WinCondition : MonoBehaviour
 
     private GameObject[] aPieces;
     private GameObject   popUpEnd;
+    private GameObject   popUpRule;
     private bool         isFinished;
 
     private void Awake()
     {
-        aPieces = GameObject.FindGameObjectsWithTag("Piece");
-        popUpEnd = GameObject.Find("PopUp");
+        aPieces   = GameObject.FindGameObjectsWithTag("Piece");
+        popUpEnd  = GameObject.Find("PopUp");
+        popUpRule = GameObject.Find("PopUpRule");
+
     }
 
     void Start()
@@ -34,9 +37,13 @@ public class WinCondition : MonoBehaviour
         {
             PlayerPrefs.SetInt("Number of clues unlocked", PlayerPrefs.GetInt("Number of clues unlocked") + 1);
             PlayerPrefs.SetInt(clueName, PlayerPrefs.GetInt("Number of clues unlocked"));
-            popUpEnd.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Cette boîte à couleurs a sans doute appartenu à Jean - Honoré Fragonard.Les flacons contiennent des pigments en poudre de différentes couleurs." + Environment.NewLine +
-                                                                                   "Pour faire de la peinture, on mélange un pigment avec un liant.Selon le type de peinture souhaité, le liant n’est pas le même : un jaune d’œuf pour faire de la peinture a tempera, de l’huile de lin pour faire de la peinture à l’huile…";
+            popUpEnd.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Cette boîte à couleurs m’a appartenu. Les flacons contiennent des pigments en poudre de différentes couleurs." + Environment.NewLine +
+                                                                                   "Pour faire de la peinture, on mélange un pigment avec un liant.Selon le type de peinture souhaité, le liant n’est pas le même : " +
+                                                                                   "un jaune d’œuf pour faire de la peinture a tempera, de l’huile de lin pour faire de la peinture à l’huile…";
             popUpEnd.SetActive(true);
+            popUpRule.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "La boîte à couleurs est complétée. Double-cliquer sur un élément pour en savoir plus." + Environment.NewLine +
+                                                                                    "Cliquer sur la flèche du bas pour revenir sur le plan et poursuivre votre parcours.";
+            popUpRule.SetActive(true);
         }
     }
 }
