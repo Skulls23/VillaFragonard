7f257a6f36942db66283e28333149260484d887b
Florian Boireau <florian23russie@hotmail.fr>
2022-09-05 13:50:13 +0200

    Removed old tests scenes and a unused script

--- Assets/Scripts/Clicker/ButtonClick.cs
+++ Assets/Scripts/Clicker/ButtonClick.cs
@@ -5,11 +5,9 @@ public class ButtonClick : MonoBehaviour
     [SerializeField] int numButton;
 
     private Clicker      clicker;
-    private ClickerPopUp clickerPopUp;
     private void Start()
     {
         clicker      = GameObject.Find("Gameplay").GetComponent<Clicker>();
-        clickerPopUp = GameObject.FindWithTag("Popup").transform.GetChild(0).GetChild(2).GetComponent<ClickerPopUp>();
     }
 
     public void Clicked()
